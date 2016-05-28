5bc439d1f6462409f2598faa6147eb1dd00fad30
Henglin Shi <Henglin.Shi@student.oulu.fi>
2016-05-28 17:03:14 +0300

    FrameReader

---
+++ KinectToolkits/FrameReader.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "FrameReader.h"
+
+
+FrameReader::FrameReader()
+{
+}
+
+
+FrameReader::FrameReader(KinectInstance * mKinect, vector <int> mFrameSourceTypes)
+{
+	if (mFrameSourceTypes.size() == 1) {
+		this->mKinect->
+	}
+	
+	else {
+		
+	}
+}
+
+FrameReader::~FrameReader()
+{
+}
